a5dbad3758ae53c0563233184424e53ee380c702
Jorge Baltodano <jbaltodanoq02@gmail.com>
2022-02-18 14:52:04 -0500

    update all soluciones

---
+++ 06-Objects/ frequencies.js
@@ -0,0 +1,28 @@
+/*
+ Write a function that accepts an array of strings, and returns an object that
+ represents the number of times that each string appears in the array. This might
+ sound confusing, but this is what we'd like to have happen.
+*/
+
+//  frequencies([ "hello", "world", "hello", "goodbye", "hello", "world", "thing" ]);
+//  => { "hello" : 3, "world" : 2, "goodbye": 1, "thing" : 1 }
+
+//     frequencies([]);     
+//=> {}
+
+//     frequencies([ "hello", "world" ]);
+//     => { "hello" : 1, "world" : 1 }
+
+// There are several ways you can do it, but it might be interesting to try it with
+// the `reduce` method that starts with an empty object.
+
+var frequencies = function (words) {
+    return words.reduce(function (frequencies, word) {
+        if (typeof frequencies[word] === "undefined") {
+            frequencies[word] = 0;
+        }
+        frequencies[word] = frequencies[word] + 1;
+        return frequencies;
+    }, {});
+};
+
---
+++ 06-Objects/isUser.js
@@ -0,0 +1,16 @@
+/*
+ Escriba una función llamada esUsuario que acepte un objeto y devuelva verdadero si es
+ un objeto de usuario válido, falso en caso contrario. Un objeto de usuario válido contendrá un
+ propiedad llamada `name` que es una cadena, y una propiedad llamada `screen_name`
+ que también es una cadena. No debe contener otras propiedades.
+*/
+
+
+// console.log(isUser({ "name":"Semmy Purewal", "screen_name":"semmypurewal" }));
+// => verdadero
+
+// console.log(isUser({ "name":"Firstname Lastname", "screen_name":"user" });
+// => verdadero
+
+// console.log(esUsuario("semmypurewal");
+// => falso
